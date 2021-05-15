2d9245755841d35825a69936a9c093d13a094140
UnrealApex <dojo1011234@gmail.com>
2021-05-14 22:15:19 -0700

    Changed alert message

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeTheme() {
+  document.getElementById("body").style.color = "white";
+  document.body.style.backgroundColor = "black";
+  alert("Welcome to the dark side");
+  // TODO: add logic for if the color theme is already the dark theme
+  // if (document.getElementById("body").style.color == "white") && (document.body.style.backgroundColor == "black") {
+  // document.getElementById("body").style.color = "black";
+  // document.body.style.backgroundColor = "white";
+
+  // }
+}
