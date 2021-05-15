98fb59f5d6ea2582138d6cebabb31e922b1b93a6
UnrealApex <dojo1011234@gmail.com>
2021-05-14 22:47:29 -0700

    Updated alerts and text

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function changeTheme() {
   document.getElementById("body").style.color = "white";
   document.body.style.backgroundColor = "black";
-  alert("Welcome to the dark side");
-  // TODO: add logic for if the color theme is already the dark theme
+  console.log("Welcome to the dark side");
+  alert("There is no return from here");
+  // TODO: add logic to toggle the theme from light to dark and vise versa
   // if (document.getElementById("body").style.color == "white") && (document.body.style.backgroundColor == "black") {
   // document.getElementById("body").style.color = "black";
   // document.body.style.backgroundColor = "white";
