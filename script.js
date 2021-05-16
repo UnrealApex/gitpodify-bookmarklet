3b9d416d0807d4c4f737c69c000e2e753a16c1e9
UnrealApex <dojo1011234@gmail.com>
2021-05-16 01:53:30 +0000

    Fixed theme not changing on third execution

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
 var darkMode = false;
-// how many times our onclick function has been called
-var callCount = 0;
 function changeTheme() {
-  callCount += 1;
   if (darkMode == false) {
+    // TODO: make "Open in Gitpod" image switch to dark theme with site
     document.getElementById("body").style.color = "white";
     document.body.style.backgroundColor = "black";
-    console.log("Dark mode enabled");
     darkMode = true;
-  }
-  else if (darkMode == true) {
-    // location.reload();
+  } else if (darkMode == true) {
     document.getElementById("body").style.color = "black";
     document.body.style.backgroundColor = "white";
-    console.log("Dark mode disabled");
-    darkMode = true;
-
-  }
-  else {
+    darkMode = false;
+  } else {
     console.log("Failed to change theme");
     darkMode = Boolean(Math.random() < 0.5);
-
   }
-  // TODO: add logic to toggle the theme from light to dark and vise versa
-  // if (document.getElementById("body").style.color == "white") && (document.body.style.backgroundColor == "black") {
-  // document.getElementById("body").style.color = "black";
-  // document.body.style.backgroundColor = "white";
-
-  // }
-
-  // the window is reloaded after 3 runs of the onclick function,
-  // this is done because after more than executions of the onclick function 
-  // it sees to not execute when clicked, this is solved by reloading the page 
-  // therefore reseting the onclick count
-  
-  if (callCount >= 3) {
-    location.reload();
-    
-   }
 }
