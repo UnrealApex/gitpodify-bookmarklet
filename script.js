e6df8d166ea4f8b51de3c964b1f5e1951d1a2860
Aaron <79121360+UnrealApex@users.noreply.github.com>
2021-09-18 13:13:27 -0700

    Refactored code

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-var isDarkMode = false;
 function toggleDarkMode() {
-    if (!isDarkMode) {
-        // TODO: Make "Open in Gitpod" image switch to dark theme with site
-        // TODO: Make dark theme more appealing
-        document.body.style.color = "#00ff00";
-        document.body.style.backgroundColor = "#121212";
-        isDarkMode = true;
-    } else if (isDarkMode) {
-        document.body.style.color = "black";
-        document.body.style.backgroundColor = "#FFFFFF";
-        isDarkMode = false;
+    if (document.body.classList.contains("light-mode")) {
+        document.body.classList.remove("light-mode");
+        document.body.classList.add("dark-mode");
+    } else if (document.body.classList.contains("dark-mode")) {
+        document.body.classList.remove("dark-mode");
+        document.body.classList.add("light-mode");
     } else {
         console.error("Failed to change theme");
-        isDarkMode = Boolean(Math.random() < 0.5);
     }
 }
